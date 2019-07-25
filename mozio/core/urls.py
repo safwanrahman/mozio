7a49bee4142cfd4bb9203eca40b8f38fead093f5
Safwan Rahman <safwan.rahman15@gmail.com>
2019-07-25 07:01:50 +0600

    fixing url

--- mozio/core/urls.py
+++ mozio/core/urls.py
@@ -7,6 +7,6 @@ app_name = "mozio.core"
 urlpatterns = [
     path('v1/users/', UserListView.as_view(), name="user-list"),
     path('v1/users/<int:pk>/', UserDetailView.as_view(), name="user-details"),
-    path('v1/service-area/', ServiceAreaListView.as_view(), name="service-list"),
-    path('v1/service-area/<int:pk>/', ServiceDetailView.as_view(), name="service-details"),
+    path('v1/service-areas/', ServiceAreaListView.as_view(), name="service-list"),
+    path('v1/service-areas/<int:pk>/', ServiceDetailView.as_view(), name="service-details"),
 ]
